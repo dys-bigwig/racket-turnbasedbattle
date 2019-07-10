811e14e57764167530177b1b963557154044c4c3
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-07-10 17:27:29 +0100

    Update battle.rkt

--- battle.rkt
+++ battle.rkt
@@ -4,22 +4,33 @@
 (require "combs.rkt")
 (require "state.rkt")
 
-(define (get-action comb)
-  (let ([actions (comb-actions comb)])
-  (if (player? comb)
-    (return (hash-ref actions
-                      (read-player-input!)))
-    (make-chance-choice actions))))
+(define (player-turn player enemy)
+  (let ([action (hash-ref (comb-actions player)
+                          (read-player-input!))])
+    (return action)))
 
-(define (make-chance-choice choices)
-  (match-define (cons chance choice)
-                (car choices))
+(define (enemy-turn enemy player)
+  (let ([actions (comb-actions enemy)])
   (do bind-state
     (rng <- get)
-    (put (drop 1 rng))
-    (if (< (first rng) chance)
-      (return choice)
-      (make-chance-choice (cdr choices)))))
+    (put (drop (length actions) rng))
+    (let ([action (make-chance-choice actions
+                                      (take (length actions) rng))])
+      (return (action enemy player))))))
+
+(define (get-action comb)
+  (let ([actions (comb-actions comb)])
+    (if (player? comb)
+      (hash-ref actions
+                (read-player-input!))
+      (make-chance-choice actions))))
+
+(define (make-chance-choice choices rands)
+  (match-define (cons chance choice)
+    (car choices))
+  (if (< (first rands) chance)
+    choice
+    (make-chance-choice (rest choices) (rest rands))))
 
 (define last-one-standing?
   (compose null? cdr))
@@ -28,12 +39,11 @@
   (match-define (cons active passive) combs)
   (cond
     [(last-one-standing? combs) (return active)]
-    [else
-      (do bind-state
-        (let ([action (get-action active)])
-          action))]))
+    [(player? active) (player-turn active passive)]
+    [(enemy? active) (enemy-turn active passive)]))
 
-(define erdrick (player "Erdrick" 10 8 (hash 1 'atk) '(asleep) 0))
-(define slib (new-slime))
 
-((battle `(,erdrick . ,slib)) (make-rng 1))
+(define erdrick (new-player))
+(define slib (new-enemy))
+
+((battle `(,slib . ,erdrick)) (make-rng 1))
